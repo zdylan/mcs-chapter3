bd1013782f364ea324d51a65ae6045edf4f258b2
dylan_zhang_ <dylan.zhang111@gmail.com>
2018-09-18 00:16:35 +0800

    first commit

---
+++ licensing-service/src/main/java/com/thoughtmechanix/licensingservice/EagleEyeLicensingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtmechanix.licensingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleEyeLicensingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EagleEyeLicensingServiceApplication.class, args);
+    }
+}
