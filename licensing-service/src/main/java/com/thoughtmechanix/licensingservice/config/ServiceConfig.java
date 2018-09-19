6100d545c19acf2875dda7338da8519f3021d508
dylan_zhang_ <dylan.zhang111@gmail.com>
2018-09-20 00:41:33 +0800

    add right example word.

--- licensing-service/src/main/java/com/thoughtmechanix/licensingservice/config/ServiceConfig.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licensingservice/config/ServiceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ServiceConfig {
 
-    @Value("${exmaple.property}")
+    @Value("${example.property}")
     private String exampleProperty;
 
     public String getExampleProperty() {
